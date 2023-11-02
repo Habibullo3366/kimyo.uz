98cb3ad08afe971baf34d6621f44209c8a7317c0
Habibullo <sayfullayevhabibullo3366@gmail.com>
2023-11-02 19:01:43 +0500

    Git init

---
+++ src/main/java/com/company/kimyouz/KimyoUzApplication.java
@@ -0,0 +1,13 @@
+package com.company.kimyouz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimyoUzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimyoUzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/kimyouz/KimyoUzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.kimyouz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimyoUzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
