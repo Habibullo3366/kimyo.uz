12e260edddf7d9f7d4285c350e82b2fbdd02792f
Fazliddin <fazik.2017@gmail.com>
2023-11-03 23:45:15 +0500

    All Mapper full crud

--- src/main/java/com/company/kimyouz/mapper/ProductMapper.java
+++ src/main/java/com/company/kimyouz/mapper/ProductMapper.java
@@ -12,13 +12,19 @@ import java.time.LocalDateTime;
 public abstract class ProductMapper {
 
     @Mapping(target = "prodId", ignore = true)
+    @Mapping(target = "categoryId", ignore = true)
+    @Mapping(target = "basketId", ignore = true)
+    @Mapping(target = "orderItemId", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "deletedAt", ignore = true)
     public abstract Product toEntity(RequestProductDto dto);
 
 
+
+    @Mapping(target = "ResponseCategoryDto",ignore = true)
     public abstract ResponseProductDto toDto(Product product);
+    public abstract ResponseProductDto toDtoWithCategories(Product product);
 
 
 
